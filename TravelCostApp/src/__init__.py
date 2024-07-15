6464f98193f2e4f6691dbbd44ae33274a2b969ed
bgreer <greergit@proton.me>
2024-07-15 06:58:46 -0500

    Refactor of app, adding flight selection process (WIP)

--- TravelCostApp/src/__init__.py
+++
@@ -1,5 +0,0 @@
-# src/__init__.py
-
-from .travel_cost_calculator import get_travel_costs
-from .google_flights_scraper import scrape_google_flights
-from .csv_handler import log_to_csv, load_csv_data
